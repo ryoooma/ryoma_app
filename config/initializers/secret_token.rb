4295a66b064279799059a1db8647e7c8ff82c75d
Ryuma Wakita <ryoma.wakita@gmail.com>
2014-05-19 13:25:03 +0900

    initialize repozitory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RyomaApp::Application.config.secret_key_base = 'd21ba7fd2053f468b5ae952caed4e7d331952d0a7005786e3bcaf6b73ca04121a9ed7eb34f483ee57af6c727b957b4400d2db1b9560d7384d1531b93a252c425'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RyomaApp::Application.config.session_store :cookie_store, key: '_ryoma_app_session'
